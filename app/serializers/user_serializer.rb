d01db87c9bb534ec29ba7500e558039944e2f98e
jmknopf1007 <jmknopf1007@gmail.com>
2020-02-24 09:07:43 -0800

    updated user serializer

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,3 +1,6 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :fullname, :username, :password_digest
+  attributes :id, :fullname, :username
+  has_many :objectives 
 end
+
+# Don't need :password_digest in serializer (not sending this information anywhere)
