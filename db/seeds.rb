874514617285f95c707379afe01da40d60d0c31e
jmknopf1007 <jmknopf1007@gmail.com>
2020-02-26 08:56:20 -0800

    updated seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,17 +18,22 @@ tia = User.create(fullname: "Tia Simmons", username: "tiann1025", password: "t",
 matt = User.create(fullname: "Matt Knopf", username: "mattman07", password: "m", password_confirmation: "m")
 
 o1 = Objective.create(title: "Learn Python", user_id: jacob.id)
-o2 = Objective.create(title: "Learn Ruby", user_id: tia.id)
-o3 = Objective.create(title: "Learn Java", user_id: matt.id)
+o2 = Objective.create(title: "Learn Spanish", user_id: jacob.id)
+o3 = Objective.create(title: "Learn Hebrew", user_id: jacob.id)
+o4 = Objective.create(title: "Learn Ruby", user_id: tia.id)
+o5 = Objective.create(title: "Learn Javascript", user_id: tia.id)
+o6 = Objective.create(title: "Learn React", user_id: tia.id)
+o7 = Objective.create(title: "Learn Java", user_id: matt.id)
+o8 = Objective.create(title: "Learn French", user_id: matt.id)
 
 g1 = Goal.create(description: "Complete 5 labs", category: "daily", day_count: 0, objective_id: o1.id)
 g2 = Goal.create(description: "Complete 3 quizzes", category: "daily", day_count: 0, objective_id: o1.id)
 g3 = Goal.create(description: "Complete 1 mini coding project", category: "daily", day_count: 0, objective_id: o1.id)
 g4 = Goal.create(description: "Complete next unit test", category: "weekly", day_count: 0, objective_id: o1.id)
-g5 = Goal.create(description: "Complete 3 labs", category: "daily", day_count: 0, objective_id: o2.id)
-g6 = Goal.create(description: "Complete 2 quizzes", category: "daily", day_count: 0, objective_id: o2.id)
-g7 = Goal.create(description: "Learn 5 new coding concepts", category: "daily", day_count: 0, objective_id: o2.id)
-g8 = Goal.create(description: "Complete next unit test", category: "weekly", day_count: 0, objective_id: o2.id)
+g5 = Goal.create(description: "Complete 3 labs", category: "daily", day_count: 0, objective_id: o4.id)
+g6 = Goal.create(description: "Complete 2 quizzes", category: "daily", day_count: 0, objective_id: o4.id)
+g7 = Goal.create(description: "Learn 5 new coding concepts", category: "daily", day_count: 0, objective_id: o4.id)
+g8 = Goal.create(description: "Complete next unit test", category: "weekly", day_count: 0, objective_id: o4.id)
 
 
 
