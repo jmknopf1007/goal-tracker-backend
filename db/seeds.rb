3079411932e718ee01e3bb0cf0c1576842bb4b3d
jmknopf1007 <jmknopf1007@gmail.com>
2020-03-27 23:45:48 -0700

    updated seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,13 +5,6 @@ Objective.reset_pk_sequence
 Goal.delete_all
 Goal.reset_pk_sequence
 
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
 
 jacob = User.create(fullname: "Jacob Knopf", username: "jmknopf1007", password: "j", password_confirmation: "j")
 tia = User.create(fullname: "Tia Simmons", username: "tiann1025", password: "t", password_confirmation: "t")
